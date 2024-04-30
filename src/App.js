41fbd96ca24d7fd6d5ae3ff1c3543f6771189ef1
Nurudeen Habibu <nurudeenh@gmail.com>
2024-04-30 23:41:41 +0000

    complete base pdas view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+import bulma from 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
   return (
     <div>
-      <div>
-        Personal Digital Assistants
+      <section className="hero is-success">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="conatiner">
+        <div className="section">
+        <div className="columns is-centered">
+            <div className="column is-3">
+              <ProfileCard  
+                title="Alexa" 
+                handle="@alexa22" 
+                image={AlexaImage}
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"/>
+            </div>
+            <div className="column is-3">
+              <ProfileCard  
+                title="Cortana" 
+                handle="@cortana99" 
+                image={CortanaImage}
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"/>
+            </div>
+            <div className="column is-3">
+              <ProfileCard  
+                title="Siri" 
+                handle="@siri01" 
+                image={SiriImage}
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"/>
+            </div>
+          </div>
+        </div>
       </div>
-      <ProfileCard  title="Alexa" handle="@alexa22"/>
-      <ProfileCard  title="Cortana" handle="@cortana99"/>
-      <ProfileCard  title="Siri" handle="@siri01"/>
+      
+      
+      
+      
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,26 @@
-function ProfileCard(props){
+function ProfileCard({title, handle, image, description}){
     return  (
-        <div>
-            <div>This is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-4by3">
+                <img
+                    src={image} 
+                    alt="Placeholder"
+                />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6">{handle}</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    {description}
+                </div>
+            </div>
         </div>
     );
 }
